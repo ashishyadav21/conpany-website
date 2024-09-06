f8467f8f88ebb76d86054f3a95fb546218f9af4e
mayank20011 <mayank.behl.mb1@gmail.com>
2024-09-06 18:23:09 +0530

    Created Technology page

---
+++ src/app/technology/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+import Technology from "@/components/Technology";
+
+export default function Home() {
+    return (
+        <main>
+            <Technology />
+        </main>
+    );
+}
